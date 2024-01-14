46882b6f6027f08dfecbe3d26c1d55bfafcc72e7
audusunday <sunnadu13@gmail.com>
2024-01-14 02:19:08 +0100

    webpack setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+	// Adding three paragraphs to the body using jQuery
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<p>Copyright - Holberton School</p>');
+});
