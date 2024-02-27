b43f415bc2654adcb46c282b1ca66148e0c4c973
audusunday <108620954+audusunday@users.noreply.github.com>
2024-02-27 11:25:29 +0100

    Delete 0x00-Webpack directory

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function () {
-	// Adding three paragraphs to the body using jQuery
-	$('body').append('<p>Holberton Dashboard</p>');
-	$('body').append('<p>Dashboard data for the students</p>');
-	$('body').append('<p>Copyright - Holberton School</p>');
-});
--- 0x00-Webpack/task_2/src/index.js
+++
@@ -1,6 +0,0 @@
-// src/index.js
-import './css/main.css';
-import { elements, updateCounter } from './dashboard_main';
-
-// Append elements to the body
-$(document.body).append(elements);
